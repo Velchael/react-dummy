204253407c4f5bd52431563fef067077e46f2969
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2022-12-29 19:40:19 -0300

    rutaspage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Users from './pages/components/Users';
+import Orders from './pages/components/Orders';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename="/react-dummy">
+      <nav>
+        <Link to="/Login">Login </Link>
+        <Link to="/Dashboard">Dashboard</Link>
+      </nav>
+      <Routes>
+        <Route exact path="/react-dummy" element={<Login />}></Route> 
+        <Route path="/dashboard" element={<Dashboard />} >
+          <Route path="users" element={<Users />}></Route> 
+          <Route path="orders" element={<Orders />}></Route> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
