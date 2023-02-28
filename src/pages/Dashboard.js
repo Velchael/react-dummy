02a1cc6ce11fc66da4f262137df896508d0b5aaf
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-02-28 00:04:38 -0300

    rutas automaticas em react-dummy

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Link, Outlet} from 'react-router-dom';
-const Dashboard = () => {
+import { useNavigate, Link, Outlet} from 'react-router-dom';
+
+function Dashboard() {
+    const navigate = useNavigate()
+
+    const goUsers = () => {
+        navigate("/Dashboard/Users")
+    }
+    const goOrders = () => {
+        navigate("/Dashboard/Orders")
+    }
     return (
         <div >
             <h1>Bienvenido al dashboard</h1>
             <p>React router dom</p>
             <hr />
             <div>
-             <Link to="/Dashboard/Users">Users </Link>
-             <Link to="/Dashboard/Orders">Orders</Link>
-             
+              <button onMouseOver={goUsers}>
+                <Link to="/Dashboard/Users">Users </Link>
+              </button>
+              <button onMouseOver={goOrders}>
+                <Link to="/Dashboard/Orders">Orders</Link>  
+              </button>
             </div>
             <Outlet />
         </div>
